bf0acb812618504b1f4fa987e98c2fcf5ccbff67
Darwin-Degreed <mdarwin@degreed.com>
2025-10-16 22:04:40 +0530

    updated for pagination logs

--- tap_jira/http.py
+++ tap_jira/http.py
@@ -297,7 +297,7 @@ class Paginator():
         params = kwargs.pop("params", {}).copy()
         while self.next_page_num is not None:
             # Always ensure defaults
-            params.setdefault("maxResults", 50)
+            params.setdefault("maxResults", 2)
             params["startAt"] = self.next_page_num
             if self.order_by:
                 params["orderBy"] = self.order_by
