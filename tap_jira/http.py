4214f42896d5a06ffd90075a63afa9e26b23827e
Darwin-Degreed <mdarwin@degreed.com>
2025-10-22 06:25:38 +0530

    fix pagination with IssuesPaginator class

--- tap_jira/http.py
+++ tap_jira/http.py
@@ -436,7 +436,7 @@ class IssuesPaginator(Paginator):
 
             LOGGER.info(f"[DEBUG PAGINATION] 🔄 Sending POST /rest/api/3/search/jql with startAt={start_at}, maxResults={max_results}")
             
-            response = self.client.request("issues", "POST", "/rest/api/3/search/jql", json=body)
+            response = self.client.request("issues", "POST", "/rest/api/3/search", json=body)
 
 
 
